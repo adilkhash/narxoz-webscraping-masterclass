42b8689c3855584e77c572be963d28818e8b1446
adilkhash <adil@playrix.com>
2017-07-27 15:26:30 +0600

    добавил пример с selenium и google

---
+++ google_selenium.py
@@ -0,0 +1,11 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+driver = webdriver.Chrome()
+driver.get("http://google.com")
+element = driver.find_element_by_name('q')
+time.sleep(3)
+element.send_keys('PYTHON')
+time.sleep(3)
+element.send_keys(Keys.RETURN)
+
